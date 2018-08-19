110ab53c43784f664e6f73ed40ef2b5ca6402475
keyuanRen <keyuanren1@gmail.com>
2018-08-20 00:17:01 +1000

    menu implementation horizontal slide content scss not working and click function not working

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,15 +1,45 @@
 
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild} from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
+  selector: 'page-menu',
   templateUrl: 'menu.html'
 })
 export class MenuPage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('swipedTabSlider') swipedTabSlider: slides;
 
+  swipedTabIndicator : any= null;
+  tabs:any=[];
+
+  constructor(public navCtrl: NavController) {
+    this.tabs=["page1", "page2", "page3", "page4"];
+  }
+
+  ionViewDidEnter()
+  {
+    this.swipedTabIndicator = document.getElementById("indicator");
+  }
+
+  selectTab(index)
+  {
+    this.swipedTabIndicator.style.webkitTransform = 'translate3d('+(100*index)+'%,0,0)';
+    this.swipedTabSlider.slideTo(index, 500);
+  }
+
+  updateIndicatorPosition()
+  {
+    if( this.swipedTabSlider.length() > this.swipedTabSlider.getActiveIndex())
+    {
+      this.swipedTabIndicator.style.webkitTransform = 'translate3d(' + (this.swipedTabSlider.getActiveIndex()*100)+'%,0,0)';
+    }
+  }
+
+  animateIndicator($event)
+  {
+    if(this.swipedTabIndicator)
+    this.swipedTabIndicator.style.webkitTransform = 'translate3d(' + ($event.progress*(this.swipedTabSlider.length()-1));
   }
 
 }
